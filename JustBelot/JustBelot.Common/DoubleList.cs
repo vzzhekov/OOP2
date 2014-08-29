7ffb9a515823b9cbf58ab990a486b96214cb0857
Vilian Zhekov <vzzhekov@gmail.com>
2014-08-29 18:16:51 +0300

    asd

---
+++ JustBelot/JustBelot.Common/DoubleList.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JustBelot.Common
+{
+    public class DoubleList <T1, T2>
+        where T1 :  IDisposable, new()
+        where T2: new()
+    {
+        private List<T1> firstList = new List<T1>();
+        private List<T2> secondList = new List<T2>();
+
+        public void Add(T1 firstValue, T2 secondValue)
+        {
+            firstValue.Dispose(); 
+            firstList.Add(firstValue);
+            secondList.Add(secondValue);
+        }
+
+        public void AddEmptyValue()
+        {
+            firstList.Add(new T1());
+            secondList.Add(new T2());
+        }
+    }
+}
